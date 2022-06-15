91c406247bb176439d9e618c51f8193130b666b6
Dnouv <sdevanshu90@yahoo.com>
2022-06-15 20:56:29 +0530

    Add Strapi init and update README

--- README.md
+++ README.md
@@ -19,25 +19,27 @@ npm link ../path/to/RC4Conferences/rc4conf
 import Head from "next/head";
 import { EventButton } from "rc4conf";
 import { Stack } from "react-bootstrap";
+import { getStrapiURL } from "../../lib/api";
 
 function Home() {
   return (
     <div>
       <Head>
-        <title>Event</title>
+        <title>Form</title>
         <meta name="description" content="Rocket.Chat form tool demo" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Stack className="mx-auto">
         <h1 className="mx-auto mt-3">Preview of Event Component</h1>
-        <EventButton bg={"yellow"} margin="auto"/>
+        <EventButton bg={"yellow"} margin="auto" strapiFunc={getStrapiURL} strapiCollName={"discourses"} />
       </Stack>
     </div>
   );
 }
 
 export default Home;
+
 
 ```
 5. Component library launched successfully!
--- rc4conf/src/components/Button.js
+++ rc4conf/src/components/Button.js
@@ -1,27 +1,62 @@
-import React, { useState } from "react"
-import axios from "axios"
+import React, { useState } from "react";
+import axios from "axios";
+import styles from "./styles/Button.module.css";
 
-export const EventButton = ({bg, margin}) => {
-    const [joke, setJoke] = useState("")
-    const handleClick = async () => {
-        const res = await axios.get("https://api.chucknorris.io/jokes/random")
-        try {
-            console.log("res", res)
-            setJoke(res.data.value)
-        }
-        catch(e) {
-            console.error("Package error", e)
-        }
+export const EventButton = ({ bg, margin, strapiFunc, strapiCollName }) => {
+  const [joke, setJoke] = useState("");
+  const handleClick = async () => {
+    try {
+      const res = await axios.get("https://api.chucknorris.io/jokes/random");
+
+      console.log("res", res);
+      setJoke(res.data.value);
+    } catch (e) {
+      console.error("Package error", e);
     }
-    return (
-        <div>
-        <button onClick={handleClick} style={{backgroundColor: bg, margin: margin}} type="warning">
-            Event, Click!
-        </button>
-        <p>
-            {joke}
-        </p>
+  };
+
+  return (
+    <div>
+      <button
+        onClick={handleClick}
+        style={{ backgroundColor: bg, margin: margin }}
+        type="warning"
+      >
+        Event, Click!
+      </button>
+      {joke && (
+        <div className={styles.jokeArea}>
+          <textarea
+            className={styles.eventButtonText}
+            type={"text"}
+            value={joke}
+            readOnly
+          ></textarea>
+          <button
+            type="danger"
+            onClick={() => strapiInit(strapiCollName, strapiFunc, joke)}
+          >
+            Save to Strapi
+          </button>
         </div>
-        
-    )
-}
+      )}
+    </div>
+  );
+};
+
+export const strapiInit = async (api, strapiFunc, data) => {
+  const requestUrl = strapiFunc(`/${api}`);
+
+  const toPost = {
+    data: {
+      TopPost: data,
+    },
+  };
+
+  try {
+    const res = await axios.post(requestUrl, toPost);
+    alert(`Data post successful with the ID - ${res.data.data.id}`);
+  } catch (e) {
+    console.error("Error while strapi init", e);
+  }
+};
