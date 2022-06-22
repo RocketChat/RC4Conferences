65e817a2651a0b514f056efa19be87a95c203621
Dnouv <sdevanshu90@yahoo.com>
2022-06-22 23:50:30 +0530

    Add conditional routing and navs

---
+++ app/components/conferences/create/EventBasicDetails.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react"
+import { Button, Card, Form } from "react-bootstrap"
+
+export const EventBasicCreate = ({setDraft}) => {
+    const [formState, setFormState] = useState({
+        "name": "",
+        "starts-at": new Date(),
+        "ends-at": new Date(),
+        "timezone": Intl.DateTimeFormat().resolvedOptions().timeZone,
+        "online": true
+    })
+
+    useEffect(() => {
+        console.log("useeff", formState)
+        const date = new Date()
+        date.setMinutes(date.getMinutes() - date.getTimezoneOffset())
+        const nativeDefault = date.toISOString().slice(0,16)
+        
+        setFormState({
+            "starts-at": nativeDefault,
+            "ends-at": nativeDefault
+        })
+    }, [])
+    console.log("after", formState)
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
+        console.log("published", formState)
+    }
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        console.log("calie", value)
+        setFormState((prev) => ({
+          ...prev,
+          [name]: value,
+        }));
+      };
+    
+    return (
+        <Card>
+            <Card.Header>Creating Event {formState.name}!</Card.Header>
+            <Card.Body>
+                <Form onSubmit={handleFormSubmit}>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Event name*</Form.Label>
+                        <Form.Control required name="name" type="text" placeholder="" onChange={handleChange} />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Start Date*</Form.Label>
+                        <Form.Control required name="starts-at" type="datetime-local" value={formState["starts-at"]} min={formState["start-at"]} onChange={handleChange} placeholder="" />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>End Date*</Form.Label>
+                        <Form.Control required name="ends-at" type="datetime-local" value={formState["ends-at"]} min={formState["start-at"]} onChange={handleChange} placeholder="" />
+                    </Form.Group>
+                    <Button variant="success" type="submit">
+                        Publish
+                    </Button>
+                </Form>
+            </Card.Body>
+        </Card>
+    )
+}
