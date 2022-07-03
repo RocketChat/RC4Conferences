891c60482184ade37b080750370bd2f3ee1d301e
Dnouv <sdevanshu90@yahoo.com>
2022-07-03 16:36:43 +0530

    Speaker responsive queries

--- app/components/conferences/display/EventShow.js
+++ app/components/conferences/display/EventShow.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import {
+  Card,
   Col,
   Container,
   Image,
@@ -11,24 +12,51 @@ import {
 } from "react-bootstrap";
 import styles from "../../../styles/event.module.css";
 import { BsCalendar2Event, BsInfoCircle } from "react-icons/bs";
-import {GoLocation} from "react-icons/go"
+import { GoLocation } from "react-icons/go";
 import { EventSpeaker } from "./EventSpeaker";
+import { useMediaQuery } from "@rocket.chat/fuselage-hooks";
 
 export const EventShow = ({ event }) => {
   const [key, setKey] = useState("home");
+  const isSmallScreen = useMediaQuery("(max-width: 576px)");
+  const isCalScreen = useMediaQuery("(min-width: 450px)");
 
   return (
     <div>
       <div className={styles.event_banner}>
         <Image src={event.data.attributes["original-image-url"]} fluid />
         <div className={styles.event_banner_title}>
-          {/* <img src={event.data.attributes["logo-url"]} width={100} /> */}
-          <h4>{event.data.attributes.name}</h4>
-          <p>by <span style={{color: "#d6162f"}}>{event.data.attributes["owner-name"]}</span></p>
+          <Container>
+            <Row>
+              {isCalScreen && <Col sm={1} xs={2}>
+              <EventSingleDate eventData={event.data} />
+              </Col>}
+              <Col>
+            <h4>{event.data.attributes.name}</h4>
+          <p>
+            by{" "}
+            <span style={{ color: "#d6162f" }}>
+              {event.data.attributes["owner-name"]}
+            </span>
+            
+          </p>
+          </Col>
+            </Row>
+          </Container>
+          {/* <h4>{event.data.attributes.name}</h4>
+          <p>
+            by{" "}
+            <span style={{ color: "#d6162f" }}>
+              {event.data.attributes["owner-name"]}
+            </span>
+          </p> */}
           <hr />
-          <Stack gap={3}>
-          <EventDate eventData={event.data} />
-          <EventLocation eventData={event.data} />
+          <Stack
+            gap={isSmallScreen ? 3 : 1}
+            direction={isSmallScreen ? "vertical" : "horizontal"}
+          >
+            <EventDate eventData={event.data} />
+            <EventLocation eventData={event.data} />
           </Stack>
         </div>
       </div>
@@ -42,15 +70,15 @@ export const EventShow = ({ event }) => {
         >
           <Tab eventKey="home" title="Overview">
             <Stack>
-            <EventDesc eventData={event.data} />
-            <Image src={event.data.attributes["logo-url"]} width={100} />
+              <EventDesc eventData={event.data} />
+              <Image src={event.data.attributes["logo-url"]} width={100} />
             </Stack>
           </Tab>
-          <Tab eventKey="sessions" title="Sessions">
-            
-          </Tab>
+          <Tab eventKey="sessions" title="Sessions"></Tab>
           <Tab eventKey="speakers" title="Speakers">
-          {key=="speakers" && <EventSpeaker eid={event.data.attributes.identifier} />}
+            {key == "speakers" && (
+              <EventSpeaker eid={event.data.attributes.identifier} />
+            )}
           </Tab>
         </Tabs>
       </div>
@@ -86,9 +114,22 @@ const EventDate = ({ eventData }) => {
   );
 };
 
-const EventLocation = ({ eventData }) => {
+const EventSingleDate = ({ eventData }) => {
+  let start = eventData.attributes["starts-at"];
+  start = new Date(start);
+  const month = start.toLocaleString("default", { month: "short" });
+  const date = start.getDate();
+  return (
+    <Stack className="mx-auto">
+      <span>{month}</span>
+      <span>{date}</span>
+      
+    </Stack>
+  );
+};
 
-  const location = eventData.attributes["location-name"]
+const EventLocation = ({ eventData }) => {
+  const location = eventData.attributes["location-name"];
 
   return (
     <Container>
@@ -98,10 +139,8 @@ const EventLocation = ({ eventData }) => {
         </Col>
         <Col>
           <div className={styles.event_date}>
-            <h6>
-              Location
-            </h6>
-            {location ? location : "Online" }
+            <h6>Location</h6>
+            {location ? location : "Online"}
           </div>
         </Col>
       </Row>
@@ -110,7 +149,6 @@ const EventLocation = ({ eventData }) => {
 };
 
 const EventDesc = ({ eventData }) => {
-
   let eventDesc = eventData.attributes.description;
   eventDesc = eventDesc.replace(/(<([^>]+)>)/gi, "");
 
@@ -118,11 +156,9 @@ const EventDesc = ({ eventData }) => {
     <Container className={styles.event_desc}>
       <Row>
         <Col>
-          <div >
+          <div>
             <h6>About the Event</h6>
-            <p>
-              {eventDesc}
-            </p>
+            <p>{eventDesc}</p>
           </div>
         </Col>
       </Row>
