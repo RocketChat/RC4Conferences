a54c6ac58e6a7f43452ad32ae0da19dcd5d1d3a5
Dnouv <sdevanshu90@yahoo.com>
2022-07-04 19:16:17 +0530

    Add speaker medium screen

--- app/components/conferences/display/EventSpeaker.js
+++ app/components/conferences/display/EventSpeaker.js
@@ -1,20 +1,25 @@
 import { useEffect, useState } from "react";
-import { Button, Col, Collapse, Container, Image, Row } from "react-bootstrap";
+import {
+  Badge,
+  Button,
+  Col,
+  Collapse,
+  Container,
+  Image,
+  Row,
+} from "react-bootstrap";
 import { getEventSpeakers } from "../../../lib/conferences/eventCall";
 import styles from "../../../styles/event.module.css";
-import { useMediaQuery } from '@rocket.chat/fuselage-hooks';
-
+import { useMediaQuery } from "@rocket.chat/fuselage-hooks";
 
 const avatarGenerate = (sn) => {
-    const res = `https://ui-avatars.com/api/?name=${sn}&background=random&size=1080`
-    return res
-}
+  const res = `https://ui-avatars.com/api/?name=${sn}&background=random&size=1080`;
+  return res;
+};
 
-export const EventSpeaker = ({ eid }) => {
+export const SmEventSpeaker = ({ eid }) => {
   const [speaker, setSpeaker] = useState(null);
   const [open, setOpen] = useState({});
-  const isSmallScreen = useMediaQuery("(max-width: 450px)");
-
 
   useEffect(async () => {
     if (!speaker) {
@@ -27,14 +32,13 @@ export const EventSpeaker = ({ eid }) => {
     setSpeaker(res.data);
   };
 
-
   const handleBioOpen = (e) => {
-    const tmod = e.target.id
+    const tmod = e.target.id;
     setOpen((prev) => ({
-        ...prev,
-        [tmod]: !open[tmod]
-    }))
-  }
+      ...prev,
+      [tmod]: !open[tmod],
+    }));
+  };
   return (
     <div>
       {speaker &&
@@ -45,7 +49,10 @@ export const EventSpeaker = ({ eid }) => {
                 <Image
                   width={100}
                   roundedCircle
-                  src={spk.attributes["photo-url"] || avatarGenerate(spk.attributes.name)}
+                  src={
+                    spk.attributes["photo-url"] ||
+                    avatarGenerate(spk.attributes.name)
+                  }
                   placeholder={avatarGenerate(spk.attributes.name)}
                 />
               </div>
@@ -57,14 +64,14 @@ export const EventSpeaker = ({ eid }) => {
                 <div className={styles.event_speaker_title}>
                   {spk.attributes.position}
                 </div>
-                <div className={styles.event_speaker_bio} >
+                <div className={styles.event_speaker_bio}>
                   {spk.attributes["short-biography"]}
                 </div>
               </Row>
               <Row className={styles.speaker_intro}>
                 <Col>
                   <Button
-                  as={"span"}
+                    as={"span"}
                     variant="light"
                     size="sm"
                     id={spk.id}
@@ -74,9 +81,95 @@ export const EventSpeaker = ({ eid }) => {
                   </Button>
                   <Collapse in={open[spk.id]}>
                     <div id="example-collapse-text">
-                      {spk.attributes["long-biography"] || "Hey, looks like this fellow speaker likes surprises."}
+                      {spk.attributes["long-biography"] ||
+                        "Hey, looks like this fellow speaker likes surprises."}
                     </div>
                   </Collapse>
+                </Col>
+              </Row>
+            </Container>
+          );
+        })}
+    </div>
+  );
+};
+
+export const MdEventSpeaker = ({ eid }) => {
+  const [speaker, setSpeaker] = useState(null);
+  const [open, setOpen] = useState({});
+
+  useEffect(async () => {
+    if (!speaker) {
+      await getSpeakers();
+    }
+  }, []);
+
+  const getSpeakers = async () => {
+    const res = await getEventSpeakers(eid);
+    setSpeaker(res.data);
+  };
+
+  const handleBioOpen = (e) => {
+    const tmod = e.target.id;
+    setOpen((prev) => ({
+      ...prev,
+      [tmod]: !open[tmod],
+    }));
+  };
+  return (
+    <div>
+      {speaker &&
+        speaker.data.map((spk) => {
+          return (
+            <Container key={spk.id} className="mb-3">
+              <Row>
+                <Col sm={2} md={2}>
+                  <div className={styles.event_speaker_avatar}>
+                    <Image
+                      fluid
+                      roundedCircle
+                      src={
+                        spk.attributes["photo-url"] ||
+                        avatarGenerate(spk.attributes.name)
+                      }
+                      placeholder={avatarGenerate(spk.attributes.name)}
+                    />
+                  </div>
+                </Col>
+                <Col>
+                  <Row>
+                    <Row className={styles.speaker_intro}>
+                      <Col md="auto" className={styles.event_speaker_name}>
+                        {spk.attributes.name}
+                      </Col>
+                      <Col md="auto" className={styles.event_speaker_title}>
+                        {spk.attributes.position}
+                      </Col>
+                    </Row>
+                    <div className={styles.event_speaker_bio}>
+                      {spk.attributes["short-biography"]}
+                      <Badge
+                        pill
+                        as={"span"}
+                        text="dark"
+                        bg="white"
+                        id={spk.id}
+                        onClick={handleBioOpen}
+                      >
+                        {`Learn More>`}
+                      </Badge>
+                    </div>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <Collapse in={open[spk.id]}>
+                        <div id="example-collapse-text">
+                          {spk.attributes["long-biography"] ||
+                            "Hey, looks like this fellow speaker likes surprises."}
+                        </div>
+                      </Collapse>
+                    </Col>
+                  </Row>
                 </Col>
               </Row>
             </Container>
