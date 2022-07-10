cc34ad636b12ee2f01221c81e2a3bb4e0cea75de
Dnouv <sdevanshu90@yahoo.com>
2022-07-11 01:38:05 +0530

    Add auto auth onclick basic

--- app/components/conferences/EventHome.js
+++ app/components/conferences/EventHome.js
@@ -2,26 +2,37 @@ import Cookies from "js-cookie";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { Button } from "react-bootstrap";
+import { autoLogin, setCookie } from "./auth/AuthHelper";
 
-const EventHome = () => {
+const EventHome = ({ passcode }) => {
   const router = useRouter();
   let session = Cookies.get("event_auth");
+  let umail = Cookies.get("user_mail");
   useEffect(() => {
     if (session) {
-    session = JSON.parse(session)
+      session = JSON.parse(session);
     }
-  })
+  });
+
+  const handleAutoAuth = async () => {
+    const res = await autoLogin(umail, passcode);
+    if (res?.data) {
+      setCookie(res);
+      router.push("/conferences/create/basic-detail")
+    }
+  };
 
   const handleClick = () => {
     if (session?.access_token) {
       router.push("/conferences/create/basic-detail");
     } else {
-      router.push("/conferences/confAuth");
+      passcode && umail
+        ? handleAutoAuth()
+        : router.push("/conferences/confAuth");
     }
   };
 
   return <Button onClick={handleClick}>Create Event!</Button>;
 };
-
 
 export default EventHome;
