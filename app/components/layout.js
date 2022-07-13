42d503ca115a4ab9ea022df250bd10a53bf7e225
Dnouv <sdevanshu90@yahoo.com>
2022-07-13 13:45:21 +0530

    Add Event role init

--- app/components/layout.js
+++ app/components/layout.js
@@ -1,26 +1,16 @@
-import '../styles/Layout.module.css';
-import Footer from './footer';
-import Menubar from './menubar';
-import { useRouter } from 'next/router';
+import "../styles/Layout.module.css";
+import Footer from "./footer";
+import Menubar from "./menubar";
+import { useRouter } from "next/router";
+import { VerifyUserRole } from "./conferences/EventAdmin";
 
 function Layout(props) {
   const { pathname } = useRouter();
   return (
     <>
-      <Menubar menu={props.menu.topNavItems} />
+      <VerifyUserRole menuprops={props} />
       {props.children}
       <Footer></Footer>
-      {/* announcement component here*/}
-      {/* pathname === '/virtualconf/mainstage' && (
-        <p className={styles.announcement}>
-          <span className={styles.announcement__svg}>
-            <BookmarkSVG />
-          </span>{' '}
-          Bookmark this page and come back on{' '}
-          <span className={styles.announcement__text}>April 6</span> for the
-          LIVE conference
-        </p>
-      )*/}
     </>
   );
 }
