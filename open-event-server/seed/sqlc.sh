c5f4629cc046d3d33b7736e5b7a85a63bea7dcd9
Dnouv <sdevanshu90@yahoo.com>
2022-08-14 21:26:45 +0530

    fix script errors in detecting the init_db file

--- open-event-server/seed/sqlc.sh
+++ open-event-server/seed/sqlc.sh
@@ -1,17 +1,25 @@
 SPEAKER_DATA=$( cat var/log/seed/speakers.json )
 EVENT_DATA=$( cat var/log/seed/event.json )
+TICKET_DATA=$( cat var/log/seed/ticket.json )
 
 printf "\nINSERT into events
 SELECT * FROM
   json_populate_record(
     null::events,
     '$EVENT_DATA'
-  );" >> var/log/seed/seed.sql
+  );" >> var/log/seed/seed_event.sql
 
 printf "\nINSERT into speaker
 SELECT * FROM
   json_populate_recordset(
     null::speaker,
     '$SPEAKER_DATA'
-  );" >> var/log/seed/seed.sql
+  );" >> var/log/seed/seed_speaker.sql
+
+printf "\nINSERT into tickets
+SELECT * FROM
+  json_populate_record(
+    null::tickets,
+    '$TICKET_DATA'
+  );" >> var/log/seed/seed_ticket.sql
 
