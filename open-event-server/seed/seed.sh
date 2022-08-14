8e096f200679167ef0480d432b1154fb8dd6b82a
Dnouv <sdevanshu90@yahoo.com>
2022-08-14 20:11:25 +0530

    Improve scripts and add flag

--- open-event-server/seed/seed.sh
+++ open-event-server/seed/seed.sh
@@ -1,2 +1,10 @@
-sh var/log/seed/sqlc.sh
+INIT_FLAG=/var/log/seed/init_db
+
+if [ ! -z $INIT_FLAG ];then
+    sh var/log/seed/sqlc.sh | exit 1
+    touch $INIT_FLAG
+else
+    echo "--Database initilalization is already done--"
+fi
+
 psql -U open_event_user open_event -f var/log/seed/seed.sql
--- startBackend.sh
+++ startBackend.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ERR_FILE=open-event-server/log/err_log.txt
+INIT_DB=open-event-server/seed/init_db
 OES_CONTAINER_ID=$( docker ps -q -f name=opev-web )
 FAUNA_CONTAINER_ID=$( docker ps -q -f name=faunadb )
 
@@ -16,7 +17,15 @@ if [ -s $ERR_FILE ];then
     echo "\033[31m***Some error occurred while starting the Open Event Server please check open-event-server/$ERR_FILE , resolve them, and then re-run the init command***\e[0m"
 fi
 if [ -z $OES_CONTAINER_ID ]; then
-    echo "\033[31m***Open-event-server Docker container was unable to install and start, please rerun the script***\e[0m"
+    echo "\033[31m***Open-event-server Docker container was unable to install and start, please rerun the script***\e[0m"    
+else
+    cd open-event-server
+    docker exec -it opev-postgres /bin/sh /var/log/seed/seed.sh
+    cd ..
+    if [ ! -z $INIT_DB ];then
+        echo echo "\033[31m***Open-event-server DB was not seeded with demo event, please check pg logs***\e[0m"
+        exit 1
+    fi
 fi
 if [ -z $FAUNA_CONTAINER_ID ]; then
     echo $FAUNA_CONTAINER_ID
