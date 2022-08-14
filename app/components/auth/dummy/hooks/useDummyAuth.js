446db896c74e67c11f7b6aac0f6172ace4163351
Dnouv <sdevanshu90@yahoo.com>
2022-08-15 02:09:44 +0530

    Improve dummy auth add some more env vars in script add validation on mainstage

--- app/components/auth/dummy/hooks/useDummyAuth.js
+++ app/components/auth/dummy/hooks/useDummyAuth.js
@@ -1,14 +1,18 @@
 import Cookies from "js-cookie";
+import Router from "next/router";
 import { useEffect, useState } from "react";
 import { signCook } from "../../../../lib/conferences/eventCall";
 
-const createDummyUser = () => {
+const createDummyUser = (role) => {
+  let email = process.env.NEXT_PUBLIC_EVENT_ADMIN_MAIL;
+  if (role === "speaker") email = process.env.NEXT_PUBLIC_EVENT_SPK_MAIL;
+  if (role === "attendee") email = process.env.NEXT_PUBLIC_EVENT_ANON_MAIL;
   return {
     id: 1,
     name: "dummy.cat",
     image:
       "https://user-images.githubusercontent.com/25859075/29918905-88dcc646-8e5c-11e7-81ec-242bc58dce1b.jpg",
-    email: process.env.NEXT_PUBLIC_EVENT_ADMIN_MAIL,
+    email: email,
     emailVerified: false,
     phoneNumber: null,
     displayName: "dummy.cat",
@@ -25,13 +29,14 @@ export const useDummyAuth = () => {
     }
   }, []);
 
-  const handleLogin = async () => {
+  const handleLogin = async (role) => {
     try {
-      const dummy_user = createDummyUser();
+      const dummy_user = createDummyUser(role);
       const hashmail = await signCook({ mail: dummy_user.email });
       setUser(dummy_user);
       sessionStorage.setItem("dummy_user", JSON.stringify(dummy_user));
       Cookies.set("hashmail", hashmail.data.hash);
+      Router.reload();
     } catch (e) {
       console.error("An error occurred while setting up user", e);
     }
@@ -41,7 +46,8 @@ export const useDummyAuth = () => {
     setUser({});
     sessionStorage.removeItem("dummy_user");
     Cookies.remove("hashmail");
-    Cookies.remove("event_auth")
+    Cookies.remove("event_auth");
+    Router.reload();
   };
 
   return {
--- app/pages/conferences/mainstage/[eid].js
+++ app/pages/conferences/mainstage/[eid].js
@@ -1,20 +1,20 @@
 import Head from "next/head";
 import { Stack } from "react-bootstrap";
-import { useRouter } from 'next/router'
-import { getEventDeatils } from "../../../lib/conferences/eventCall";
+import { useRouter } from "next/router";
+import { getEventDeatils, unsignCook } from "../../../lib/conferences/eventCall";
 import { EventShow } from "../../../components/conferences/display/EventShow";
 import { EventMainstage } from "../../../components/conferences/dayOfEvent/mainstage/Mainstage";
 
-const EventMainstagePage = ({event}) => {
+const EventMainstagePage = ({ event }) => {
   return (
     <div>
       <Head>
-          <title>Virtual Conference Main Stage</title>
-          <meta
-            name="description"
-            content="Demonstration main stage for a virtual conference"
-          />
-        </Head>
+        <title>Virtual Conference Main Stage</title>
+        <meta
+          name="description"
+          content="Demonstration main stage for a virtual conference"
+        />
+      </Head>
       <div className="mx-auto">
         <Stack direction="vertical">
           <EventMainstage eventdetails={event} />
@@ -22,16 +22,40 @@ const EventMainstagePage = ({event}) => {
       </div>
     </div>
   );
-}
+};
 
 export async function getServerSideProps(context) {
-  const eventIdentifier = context.query.eid
+  const eventIdentifier = context.query.eid;
   //temp 9ddffcbb
-    // const res = await getEventDeatils(eventIdentifier)
-    // const event = res.data
+  // const res = await getEventDeatils(eventIdentifier)
+  // const event = res.data
+  const umail = context.req.cookies?.hashmail;
+  if (!umail) {
     return {
-      props: { eventIdentifier },
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
     };
   }
+  const mailres = await unsignCook({
+    hash: umail,
+  });
+
+  const emailRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    if (!emailRegex.test(mailres.data.mail)) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: false,
+        },
+      };
+    }
+    return {
+    props: { eventIdentifier },
+  };
+}
 
 export default EventMainstagePage;
