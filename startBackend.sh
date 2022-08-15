d82f7b6aca8469c5770ad2e05ae048f8f488855e
Dnouv <sdevanshu90@yahoo.com>
2022-08-15 12:15:57 +0530

    Add exit codes

--- startBackend.sh
+++ startBackend.sh
@@ -15,9 +15,11 @@ cd ..
 
 if [ -s $ERR_FILE ];then
     echo "\033[31m***Some error occurred while starting the Open Event Server please check open-event-server/$ERR_FILE , resolve them, and then re-run the init command***\e[0m"
+    exit 1
 fi
 if [ -z $OES_CONTAINER_ID ]; then
     echo "\033[31m***Open-event-server Docker container was unable to install and start, please rerun the script***\e[0m"    
+    exit 1
 else
     cd open-event-server
     docker exec -it opev-postgres /bin/sh /var/log/seed/seed.sh
@@ -32,4 +34,5 @@ fi
 if [ -z $FAUNA_CONTAINER_ID ]; then
     echo $FAUNA_CONTAINER_ID
     echo "\033[31m***FaunaDB container was unable to install and start, please rerun the script***\e[0m"
+    exit 1
 fi
