4ec67d9cf59f1af302f8ecc0cd5eea054e8d8e4e
Dnouv <sdevanshu90@yahoo.com>
2022-08-18 21:42:47 +0530

    Add a event delete button

--- app/components/conferences/admin/dashboard.js
+++ app/components/conferences/admin/dashboard.js
@@ -1,7 +1,12 @@
 import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
 import { Button, ButtonGroup, ListGroup } from "react-bootstrap";
-import { getUserEventDeatils } from "../../../lib/conferences/eventCall";
+import { BiEdit } from "react-icons/bi";
+import { MdDelete } from "react-icons/md";
+import {
+  deleteEvent,
+  getUserEventDeatils,
+} from "../../../lib/conferences/eventCall";
 import styles from "../../../styles/event.module.css";
 
 export const EventDashBoard = () => {
@@ -24,6 +29,19 @@ export const EventDashBoard = () => {
     }
   }, []);
 
+  const handleDelete = async (e) => {
+    try {
+      await deleteEvent(e.target.id, authCookie?.access_token);
+      const eventres = await getUserEventDeatils(
+        authCookie.jwtInfo.identity,
+        authCookie.access_token
+      );
+      setEventData(eventres.data);
+    } catch (e) {
+      console.error("An error occurred while deleting the Event", e);
+    }
+  };
+
   return (
     <ListGroup className={styles.admin_event_list}>
       {eventData?.data &&
@@ -32,8 +50,17 @@ export const EventDashBoard = () => {
             <ListGroup.Item key={event.id} className={styles.admin_event_item}>
               <div>{event.attributes.name}</div>
               <ButtonGroup>
-                <Button variant="secondary" target="_blank" href={`/conferences/c/${event.id}`}>Preview</Button>
-                <Button href={`/conferences/admin/c/${event.id}`}>Edit</Button>
+                <Button
+                  variant="secondary"
+                  target="_blank"
+                  href={`/conferences/c/${event.id}`}
+                >
+                  Preview
+                </Button>
+                <Button href={`/conferences/admin/c/${event.id}`}>{<BiEdit />}</Button>
+                <Button variant={"danger"} id={event.id} onClick={handleDelete}>
+                  {<MdDelete id={event.id} onClick={(e) => handleDelete(e)} />}
+                </Button>
               </ButtonGroup>
             </ListGroup.Item>
           );
