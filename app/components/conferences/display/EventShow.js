8e960ed86efd6e63520eda6aa938cc07090e4124
Dnouv <sdevanshu90@yahoo.com>
2022-08-26 21:49:03 +0530

    Add session exapnd info

--- app/components/conferences/display/EventShow.js
+++ app/components/conferences/display/EventShow.js
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import {
+  Badge,
+  Button,
   Card,
   Col,
+  Collapse,
   Container,
   Image,
   Row,
   Stack,
   Tab,
+  Table,
   Tabs,
 } from "react-bootstrap";
 import styles from "../../../styles/event.module.css";
@@ -16,9 +20,12 @@ import { useMediaQuery } from "@rocket.chat/fuselage-hooks";
 import { MdEventHeader, SmEventHeader } from "./EventHeader";
 import Cookies from "js-cookie";
 import { unsignCook } from "../../../lib/conferences/eventCall";
+import { BiChevronDown, BiExpand } from "react-icons/bi";
 
-export const EventShow = ({ event, error, speaker }) => {
+export const EventShow = ({ event, error, speaker, prsession }) => {
   const [key, setKey] = useState("home");
+  const [toOpen, setToOpen] = useState({});
+
   const isSmallScreen = useMediaQuery("(max-width: 576px)");
   const [isSignedIn, setIsSignedIn] = useState(false);
 
@@ -68,13 +75,25 @@ export const EventShow = ({ event, error, speaker }) => {
               </div>
             </Stack>
           </Tab>
-          <Tab eventKey="sessions" title="Sessions"></Tab>
+          <Tab eventKey="sessions" title="Sessions">
+            <EventSession
+              session={prsession}
+              toOpen={toOpen}
+              setToOpen={setToOpen}
+            />
+          </Tab>
           <Tab eventKey="speakers" title="Speakers">
             {key == "speakers" &&
               (isSmallScreen ? (
-                <SmEventSpeaker eid={event.data.attributes.identifier} speaker={speaker} />
+                <SmEventSpeaker
+                  eid={event.data.attributes.identifier}
+                  speaker={speaker}
+                />
               ) : (
-                <MdEventSpeaker eid={event.data.attributes.identifier} speaker={speaker} />
+                <MdEventSpeaker
+                  eid={event.data.attributes.identifier}
+                  speaker={speaker}
+                />
               ))}
           </Tab>
         </Tabs>
@@ -97,6 +116,91 @@ const EventDesc = ({ eventData }) => {
           </div>
         </Col>
       </Row>
+    </Container>
+  );
+};
+
+const EventSession = ({ session, toOpen, setToOpen }) => {
+  const helperHead = {
+    start_time: "Time",
+    presenter: "Presenter",
+    mentor: "Mentor",
+    presentation_title: "Presentation Title",
+    duration_minutes: "Duration Minutes",
+  };
+
+
+  const retHours = (tm) => {
+    const tmToDate = new Date(tm);
+    return tmToDate.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  };
+
+  const handleSessionExpand = (e) => {
+    const tmod = e.target.id;
+    setToOpen((prev) => ({
+      ...prev,
+      [tmod]: !toOpen[tmod],
+    }));
+  };
+  return (
+    <Container>
+      <Table responsive striped hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th></th>
+            {Object.values(helperHead).map((hitem, ind) => {
+              return <th key={ind}>{hitem}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(session) ? (
+            session.map((sess) => {
+              return (
+                <>
+                  <tr key={sess.id}>
+                    <td>
+                      <Badge
+                        size="sm"
+                        id={sess.id}
+                        onClick={handleSessionExpand}
+                        bg="light" text="dark"
+                        className={styles.session_expand}
+                      >
+                        {">"}
+                      </Badge>
+                    </td>
+                    <td>
+                      <Badge pill bg="warning">
+                        {" "}
+                      </Badge>
+                    </td>
+                    <td>
+                      {retHours(sess.attributes.start_time)} -{" "}
+                      {retHours(sess.attributes.end_time)}
+                    </td>
+                    <td>{sess.attributes.presenter}</td>
+                    <td>{sess.attributes.mentor}</td>
+                    <td>{sess.attributes.presentation_title}</td>
+                    <td>{sess.attributes.duration_minutes}</td>
+                  </tr>
+                  <tr>
+                    <Collapse in={toOpen[sess.id]}>
+                      <td colspan="12">{`Descsription: ${sess.attributes.description}`}</td>
+                    </Collapse>
+                  </tr>
+                </>
+              );
+            })
+          ) : (
+            <tr></tr>
+          )}
+        </tbody>
+      </Table>
     </Container>
   );
 };
