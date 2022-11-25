19fe0a81e6d88aee54434b6f2c28f0b5144d1752
Dnouv <sdevanshu90@yahoo.com>
2022-11-25 14:33:01 +0000

    A minor if loop condition check

--- app/pages/api/conf/unsignCook.js
+++ app/pages/api/conf/unsignCook.js
@@ -1,24 +1,27 @@
 import crypto from "crypto-js";
 
 export default function handler(req, res) {
-    if (req.method === 'POST') {
-      let hashObject = undefined
-      if (typeof req.body == Object) {
-        hashObject = req.body
-      }
-      if (typeof req.body == "string") {
-        hashObject = JSON.parse(req.body)
-      }
-      if (!hashObject?.hash) {
-        res.status(401).json({error: "Not Authorized!"})
-        res.end()
-      }
-        const decrypted = crypto.AES.decrypt(hashObject?.hash, process.env.EVENT_USER_PASSPHRASE)
-        res.status(200).json({
-            mail: decrypted.toString(crypto.enc.Utf8),
-        })
-      } else {
-        // Handle any other HTTP method
-        res.status(400).json({error: "Unsupported Route Method"})
-      }
+  if (req.method === "POST") {
+    let hashObject = undefined;
+    if (typeof req.body == "object") {
+      hashObject = req.body;
+    }
+    if (typeof req.body == "string") {
+      hashObject = JSON.parse(req.body);
+    }
+    if (!hashObject?.hash) {
+      res.status(401).json({ error: "Not Authorized!" });
+      res.end();
+    }
+    const decrypted = crypto.AES.decrypt(
+      hashObject?.hash,
+      process.env.EVENT_USER_PASSPHRASE
+    );
+    res.status(200).json({
+      mail: decrypted.toString(crypto.enc.Utf8),
+    });
+  } else {
+    // Handle any other HTTP method
+    res.status(400).json({ error: "Unsupported Route Method" });
   }
+}
