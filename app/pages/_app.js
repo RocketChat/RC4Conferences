2f1a137848e3750b4c3caa0ae35da78f716edfcf
デヴぁんす <61188295+Dnouv@users.noreply.github.com>
2023-01-14 19:51:25 +0530

    [NEW] Remove error boundary (#84)
    * remove error bounday
    
    * remove unused impor

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -5,12 +5,10 @@ import SSRProvider from 'react-bootstrap/SSRProvider';
 import {SessionProvider} from 'next-auth/react';
 import { ApolloProvider } from '@apollo/client';
 import client from '../apollo-client';
-import ErrorBoundary from '../components/error';
 
 function MyApp({ Component, pageProps: {session, ...pageProps}}) {
   return (
     <SSRProvider>
-      <ErrorBoundary>
       <ApolloProvider client={client}>
       <SessionProvider session={session}>
         <Layout menu={pageProps}>
@@ -18,7 +16,6 @@ function MyApp({ Component, pageProps: {session, ...pageProps}}) {
         </Layout>
         </SessionProvider>
       </ApolloProvider>
-      </ErrorBoundary>
     </SSRProvider>
   );
 }
