33813e9681f46c2f0bcf7eaffb1003bdffbaae1e
Dnouv <sdevanshu90@yahoo.com>
2023-01-18 12:06:01 +0530

    fix mainstage popup warning

--- app/components/conferences/dayOfEvent/mainstage/videostreamer.js
+++ app/components/conferences/dayOfEvent/mainstage/videostreamer.js
@@ -6,26 +6,36 @@ import { useEffect, useState } from "react";
 
 export default function Videostreamer(props) {
   const [ping, setPing] = useState(false);
-
-  const pingStream = async () => {
-    const response = await fetch(props.src);
-    if (response.ok) {
-      setPing(true);
-    }
-
-    return response;
-  };
-  useEffect(() => {
-    setInterval(async () => {
-      pingStream().catch((e) => {
-        console.error("Stream error", e);
-        setPing(false);
-        return;
-      });
-    }, 30000);
-  }, []);
+  const [message, setMessage] = useState(
+    "Thanks for joining in! Please start the stream."
+  );
 
   const handleToast = () => {
+    setPing(true);
+  };
+
+  const handleWait = () => {
+    setPing(false);
+    setMessage("Thanks for watching! Stream will be back soon.");
+  };
+
+  const handleStall = () => {
+    setPing(false);
+    setMessage("Thanks for watching! Failed to fetch data, but trying.");
+  };
+
+  const handleProgress = () => {
+    setPing(true);
+  };
+
+  const handleError = () => {
+    setPing(false);
+    setMessage(
+      "Thanks for watching! Stream is not available. Please try again later or contact organizer."
+    );
+  };
+
+  const handleLoad = () => {
     setPing(true);
   };
 
@@ -46,16 +56,23 @@ export default function Videostreamer(props) {
       />
       <Col className={styles.video_root}>
         <div className={styles.video_server}>
-          <Badge pill bg={"warning"} text={"black"}>{props.region}</Badge>
-          </div>
+          <Badge pill bg={"warning"} text={"black"}>
+            {props.region}
+          </Badge>
+        </div>
         <video
-          autoPlay
+          autoPlay={true}
           id={styles.my_video}
           className="video-js vjs-big-play-centered vjs-responsive"
           controls
           preload="auto"
           poster={props.poster}
           data-setup='{}'
+          onError={handleError}
+          onProgress={handleProgress}
+          onLoadedData={handleLoad}
+          onWaiting={handleWait}
+          onStalled={handleStall}
         >
           <source src={props.src} type={props.type}></source>
           <p className="vjs-no-js">
@@ -66,13 +83,13 @@ export default function Videostreamer(props) {
             </a>
           </p>
         </video>
-        <Alert show={!ping} handleToast={handleToast} />
+        <Alert show={!ping} handleToast={handleToast} message={message} />
       </Col>
     </>
   );
 }
 
-const Alert = ({ handleToast, show }) => {
+const Alert = ({ handleToast, show, message }) => {
   return (
     <ToastContainer
       position="bottom-start"
@@ -89,10 +106,7 @@ const Alert = ({ handleToast, show }) => {
         <Toast.Header>
           <strong className="me-auto">Stream Alert!</strong>
         </Toast.Header>
-        <Toast.Body>
-          Thank you for watching! Looks like the streaming has stopped! Please
-          stay tune and refresh the page if this alert does not shows up!
-        </Toast.Body>
+        <Toast.Body>{message}</Toast.Body>
       </Toast>
     </ToastContainer>
   );
