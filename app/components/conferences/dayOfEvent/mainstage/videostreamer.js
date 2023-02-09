7421e5e2e6063daf0d6d4818e94dbfe8149ca759
Vanshul Bhatia <98731471+VanshulB@users.noreply.github.com>
2023-02-10 02:38:00 +0530

    twitch and vimeo support to videostream component (#93)
    * twitch and vimeo support to videostram component
    
    * adding responsiveness to embedded player
    
    * adding style and documentation
    
    * adding wiki documentation for videostream component
    
    * Adding demo video to wiki docs
    
    * parent key handling for production
    
    * env variable for parent key

--- app/components/conferences/dayOfEvent/mainstage/videostreamer.js
+++ app/components/conferences/dayOfEvent/mainstage/videostreamer.js
@@ -39,52 +39,144 @@ export default function Videostreamer(props) {
     setPing(true);
   };
 
+  let parentKey = [];
+  if (process.env.NEXT_PUBLIC_TWITCH_PARENT_KEY !== undefined) {
+    parentKey = process.env.NEXT_PUBLIC_TWITCH_PARENT_KEY.split(",");
+  }
+
   return (
     <>
-      <Head>
-        <link
-          href="https://vjs.zencdn.net/7.17.0/video-js.css"
-          rel="stylesheet"
-        />
-      </Head>
-      <Script
-        src="https://vjs.zencdn.net/7.17.0/video.min.js"
-        strategy="afterInteractive"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
-      <Col className={styles.video_root}>
-        <div className={styles.video_server}>
-          <Badge pill bg={"warning"} text={"black"}>
-            {props.region}
-          </Badge>
-        </div>
-        <video
-          autoPlay={true}
-          id={styles.my_video}
-          className="video-js vjs-big-play-centered vjs-responsive"
-          controls
-          preload="auto"
-          poster={props.poster}
-          data-setup='{}'
-          onError={handleError}
-          onProgress={handleProgress}
-          onLoadedData={handleLoad}
-          onWaiting={handleWait}
-          onStalled={handleStall}
-        >
-          <source src={props.src} type={props.type}></source>
-          <p className="vjs-no-js">
-            To view this video please enable JavaScript, and consider upgrading
-            to a web browser that
-            <a href="https://videojs.com/html5-video-support/" target="_blank">
-              supports HTML5 video
-            </a>
-          </p>
-        </video>
-        <Alert show={!ping} handleToast={handleToast} message={message} />
-      </Col>
+      {props.service === "local" && (
+        <>
+          <Head>
+            <link
+              href="https://vjs.zencdn.net/7.17.0/video-js.css"
+              rel="stylesheet"
+            />
+          </Head>
+          <Script
+            src="https://vjs.zencdn.net/7.17.0/video.min.js"
+            strategy="afterInteractive"
+            onLoad={() =>
+              console.log(
+                `script loaded correctly, window.FB has been populated`
+              )
+            }
+          />
+          <Col className={styles.video_root}>
+            <div className={styles.video_server}>
+              <Badge pill bg={"warning"} text={"black"}>
+                {props.region}
+              </Badge>
+            </div>
+            <video
+              autoPlay={true}
+              id={styles.my_video}
+              className="video-js vjs-big-play-centered vjs-responsive"
+              controls
+              preload="auto"
+              poster={props.poster}
+              data-setup="{}"
+              onError={handleError}
+              onProgress={handleProgress}
+              onLoadedData={handleLoad}
+              onWaiting={handleWait}
+              onStalled={handleStall}
+            >
+              <source src={props.src} type={props.type}></source>
+              <p className="vjs-no-js">
+                To view this video please enable JavaScript, and consider
+                upgrading to a web browser that
+                <a
+                  href="https://videojs.com/html5-video-support/"
+                  target="_blank"
+                >
+                  supports HTML5 video
+                </a>
+              </p>
+            </video>
+            <Alert show={!ping} handleToast={handleToast} message={message} />
+          </Col>
+        </>
+      )}
+      {props.service === "twitch" && (
+        <>
+          <div
+            className="twitch-container"
+            style={{ margin: 20, width: "90%" }}
+          >
+            <div
+              id="twitch-embed"
+              style={{
+                position: "relative",
+                paddingTop: "50%",
+                overflow: "hidden",
+              }}
+            ></div>
+          </div>
+          <Script
+            src="https://embed.twitch.tv/embed/v1.js"
+            strategy={"afterInteractive"}
+            onLoad={() => {
+              console.log("succesfully loaded twitch.tv script");
+              const embed = new Twitch.Embed("twitch-embed", {
+                width: "100%",
+                height: "100%",
+                channel: props.src,
+                layout: "video",
+                autoplay: false,
+                parent:
+                  process.env.NODE_ENV === "production"
+                    ? process.env.NEXT_PUBLIC_TWITCH_PARENT_KEY.split(",")
+                    : parentKey.length > 0
+                    ? parentKey
+                    : ["localhost", "127.0.0.1"],
+              });
+              embed.setVolume(0.5);
+              var iframe = document
+                .getElementById("twitch-embed")
+                .getElementsByTagName("iframe")[0];
+              iframe.style.cssText =
+                "top: 0; left: 0; border: none; position: absolute; width: 100%; height: 100%;";
+              embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
+                const player = embed.getPlayer();
+                player.play();
+              });
+            }}
+          />
+        </>
+      )}
+      {props.service === "vimeo" && (
+        <>
+          <div className="vimeo-container" style={{ margin: 20, width: "90%" }}>
+            <div
+              id="vimeo-embed"
+              style={{
+                position: "relative",
+                paddingTop: "50%",
+                overflow: "hidden",
+              }}
+            >
+              <iframe
+                src={props.src}
+                width="100%"
+                height="100%"
+                frameBorder="0"
+                style={{
+                  top: 0,
+                  left: 0,
+                  border: "none",
+                  position: "absolute",
+                  width: "100%",
+                  height: "100%",
+                }}
+                allow="autoplay; fullscreen"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
