1e36b011393a44144a0fb85bba0447a349dc87b0
Sarvesh Limaye <sarveshlimaye2002@gmail.com>
2023-02-10 08:35:33 +0000

    Added toast component

---
+++ app/components/toast/ui/ShowToast.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect} from 'react';
+import Alert from 'react-bootstrap/Alert';
+import styles from '../styles/ShowToast.module.css'
+
+export default function ShowToast(props) {
+  const [show, setShow] = useState(true);
+  return (
+    <div className={styles.alert}>
+     {show && <Alert variant={props.status === 'error' ? 'danger' : 'success'} onClose={() => setShow(false)} dismissible>
+      {props.msg}
+     </Alert>}
+    </div>
+  );
+}
