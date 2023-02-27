cf844bf365e35cb799b85e42d202d3b0618a15da
Aashray Katiyar <aashray446@gmail.com>
2023-02-27 12:05:41 +0530

    [NEW] Add loading feedback screen on routing  * Add loader css in globals.css for top nav loader  * Add logic to detect router change at layout.js

--- app/components/layout.js
+++ app/components/layout.js
@@ -1,15 +1,23 @@
 import "../styles/Layout.module.css";
 import Footer from "./footer";
-import { useRouter } from "next/router";
+import { useRouter, Router } from "next/router";
 import { VerifyUserRole } from "./conferences/EventAdmin";
+import { useState } from "react";
 
 function Layout(props) {
+  const [loading, setLoading] = useState(false);
+
+  Router.events.on('routeChangeStart', () => setLoading(true))
+  Router.events.on('routeChangeComplete', () => setLoading(false))
+
+
   const { pathname } = useRouter();
   return (
     <>
       <VerifyUserRole menuprops={props} />
       {props.children}
       <Footer></Footer>
+      {loading && <span className='loader'></span>}
     </>
   );
 }
