7aba29d9164a9d907d3ce25309c4675c0e115074
デヴぁんす <sdevanshu90@yahoo.com>
2023-03-21 23:19:29 +0530

    try 4

--- open-event-server/seed/seed.sh
+++ open-event-server/seed/seed.sh
@@ -4,8 +4,7 @@ if [ ! -e $INIT_FLAG ];then
     sh var/log/seed/sqlc.sh | exit 1
     
     echo "--Cleaning Up the database--"
-    docker ps
-    psql -U open_event_user open_event -f var/log/seed/seed.sql
+    psql -h localhost -U open_event_user open_event -f var/log/seed/seed.sql
     echo "--Creating SQL files--"
     psql -U open_event_user open_event -f var/log/seed/tmp/seed_event.sql
     psql -U open_event_user open_event -f var/log/seed/tmp/seed_speaker.sql
