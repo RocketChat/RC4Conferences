49dce304bb6b5d869cd2caca06429bf5c1f91dde
デヴぁんす <sdevanshu90@yahoo.com>
2024-03-21 12:36:34 +0530

    update embedded chat

--- app/components/conferences/dayOfEvent/greenroom/EventSpeakerRoom.js
+++ app/components/conferences/dayOfEvent/greenroom/EventSpeakerRoom.js
@@ -1,19 +1,14 @@
-import styles from "../../../../styles/event.module.css";
-import { DoEWrapper } from "../wrapperComponent";
-import { SpeakerChatToolbar } from "./SpeakerToolbar";
-import { Card, Collapse } from "react-bootstrap";
-import { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
-import { useMediaQuery } from "@rocket.chat/fuselage-hooks";
-import { ssrVerifyAdmin } from "../../auth/AuthSuperProfileHelper";
-import { unsignCook } from "../../../../lib/conferences/eventCall";
-import Cookies from "js-cookie";
-import Jitsibroadcaster from "./Jitsibroadcaster";
-
-const RCComponent = dynamic(
-  () => import("rc-component-react").then((mod) => mod.RCComponent),
-  { ssr: false }
-);
+import styles from '../../../../styles/event.module.css';
+import { DoEWrapper } from '../wrapperComponent';
+import { SpeakerChatToolbar } from './SpeakerToolbar';
+import { Card, Collapse } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+import { useMediaQuery } from '@rocket.chat/fuselage-hooks';
+import { ssrVerifyAdmin } from '../../auth/AuthSuperProfileHelper';
+import { unsignCook } from '../../../../lib/conferences/eventCall';
+import Cookies from 'js-cookie';
+import Jitsibroadcaster from './Jitsibroadcaster';
 
 const EventSpeakerStage = ({
   spkdata,
@@ -22,14 +17,14 @@ const EventSpeakerStage = ({
   open,
   setOpen,
 }) => {
-  const isSmallScreen = useMediaQuery("(max-width: 790px)");
+  const isSmallScreen = useMediaQuery('(max-width: 790px)');
 
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     const verifyAdminAccess = async () => {
       try {
-        const hashmail = Cookies.get("hashmail");
+        const hashmail = Cookies.get('hashmail');
 
         const res = await unsignCook({ hash: hashmail });
         const mail = res.mail;
@@ -39,7 +34,7 @@ const EventSpeakerStage = ({
           setIsAdmin(isAdminRes);
         }
       } catch (e) {
-        console.error("An error while verifying admin access", e);
+        console.error('An error while verifying admin access', e);
       }
     };
     verifyAdminAccess();
@@ -47,20 +42,20 @@ const EventSpeakerStage = ({
 
   return (
     <div>
-        <div className={styles.greenroom_jitsi}>
-          <Jitsibroadcaster
-            room={
-              eventdata
-                ? eventdata.data.attributes?.["chat-room-name"]
-                : `DemoDay-${eventIdentifier}`
-            }
-            disName={"Speakers"}
-            isAdmin={isAdmin}
-            eventData={eventdata}
-            open={open}
-            setOpen={setOpen}
-          />
-        </div>
+      <div className={styles.greenroom_jitsi}>
+        <Jitsibroadcaster
+          room={
+            eventdata
+              ? eventdata.data.attributes?.['chat-room-name']
+              : `DemoDay-${eventIdentifier}`
+          }
+          disName={'Speakers'}
+          isAdmin={isAdmin}
+          eventData={eventdata}
+          open={open}
+          setOpen={setOpen}
+        />
+      </div>
     </div>
   );
 };
--- app/components/conferences/dayOfEvent/mainstage/Mainstage.js
+++ app/components/conferences/dayOfEvent/mainstage/Mainstage.js
@@ -1,43 +1,38 @@
-import { useEffect, useState } from "react";
-import { Button, Col, Collapse, Container } from "react-bootstrap";
-import Videostreamer from "./videostreamer";
+import { useEffect, useState } from 'react';
+import { Button, Col, Collapse, Container } from 'react-bootstrap';
+import Videostreamer from './videostreamer';
 // import { SpeakerChatToolbar } from "../greenroom/SpeakerToolbar";
-import { DoEWrapper } from "../wrapperComponent";
+import { DoEWrapper } from '../wrapperComponent';
 
-import styles from "../../../../styles/event.module.css";
-import dynamic from "next/dynamic";
-import { getIPInfo } from "../../../../lib/geoAPI";
-import { useMediaQuery } from "@rocket.chat/fuselage-hooks";
-import { EventHeader } from "../greenroom/EventHeader";
-
-const RCComponent = dynamic(
-  () => import("rc-component-react").then((mod) => mod.RCComponent),
-  { ssr: false }
-);
+import styles from '../../../../styles/event.module.css';
+import dynamic from 'next/dynamic';
+import { getIPInfo } from '../../../../lib/geoAPI';
+import { useMediaQuery } from '@rocket.chat/fuselage-hooks';
+import { EventHeader } from '../greenroom/EventHeader';
 
 const asiaLink = process.env.NEXT_PUBLIC_SERVER_STREAM_LINK0;
 const otherLink = process.env.NEXT_PUBLIC_SERVER_STREAM_LINK1;
 
 export const EventMainstage = ({ eventdetails, open, setOpen }) => {
-  const isSmallScreen = useMediaQuery("(max-width: 790px)");
+  const isSmallScreen = useMediaQuery('(max-width: 790px)');
   // const [open, setOpen] = useState(isSmallScreen);
   const [streamLink, setStreamLink] = useState(asiaLink);
-  const [region, setRegion] = useState("Asia");
-  const evePoster = eventdetails?.data?.attributes?.["original-image-url"];
+  const [region, setRegion] = useState('Asia');
+  const evePoster = eventdetails?.data?.attributes?.['original-image-url'];
 
   useEffect(async () => {
     try {
       const res = await getIPInfo();
       const ipInfo = res.data;
-      if (ipInfo.timezone.split("/")[0] == "Asia") {
+      if (ipInfo.timezone.split('/')[0] == 'Asia') {
         setStreamLink(asiaLink);
-        setRegion(ipInfo.timezone.split("/")[0]);
+        setRegion(ipInfo.timezone.split('/')[0]);
       } else {
         setStreamLink(otherLink);
-        setRegion(ipInfo.timezone.split("/")[0]);
+        setRegion(ipInfo.timezone.split('/')[0]);
       }
     } catch (e) {
-      console.error("error in ip allocation switching to Asia server", e);
+      console.error('error in ip allocation switching to Asia server', e);
     }
   }, []);
 
@@ -45,8 +40,8 @@ export const EventMainstage = ({ eventdetails, open, setOpen }) => {
     <div className={styles.mainstage_root}>
       <EventHeader eventData={eventdetails} open={open} setOpen={setOpen} />
       <Videostreamer
-        poster={evePoster ? evePoster : "/gsocsmall.jpg"}
-        src={"streamURL"}
+        poster={evePoster ? evePoster : '/gsocsmall.jpg'}
+        src={'streamURL'}
         service="local"
         type="application/vnd.apple.mpegurl"
         region={region}
