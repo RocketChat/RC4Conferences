5f260aba4c233e16db6c0364c228faf360827a6b
デヴぁんす <sdevanshu90@yahoo.com>
2024-03-21 12:36:34 +0530

    update embedded chat

--- app/components/conferences/dayOfEvent/RCchat.js
+++ app/components/conferences/dayOfEvent/RCchat.js
@@ -1,11 +1,9 @@
-import dynamic from "next/dynamic";
-import { useState } from "react";
-import { verifyValidRCchat } from "./helper";
-
-const RCComponent = dynamic(
-  () => import("rc-component-react").then((mod) => mod.RCComponent),
-  { ssr: false }
-);
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import { verifyValidRCchat } from './helper';
+const EmbeddedChat = dynamic(() => import('@embeddedchat/react'), {
+  ssr: false,
+});
 
 export const RCdesktopChat = ({ open, setOpen }) => {
   const validRCflag = verifyValidRCchat();
@@ -13,18 +11,18 @@ export const RCdesktopChat = ({ open, setOpen }) => {
   return (
     <>
       {open && validRCflag.valid ? (
-        <RCComponent
+        <EmbeddedChat
           moreOpts={true}
           isClosable={true}
           setClosableState={setOpen}
           width="auto"
-          height={"55vh"}
+          height={'55vh'}
           GOOGLE_CLIENT_ID={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
           host={process.env.NEXT_PUBLIC_RC_URL}
           roomId={
             process.env.NEXT_PUBLIC_RC_ROOM_ID
               ? process.env.NEXT_PUBLIC_RC_ROOM_ID
-              : "GENERAL"
+              : 'GENERAL'
           }
           channelName="General"
           anonymousMode={true}
@@ -45,13 +43,13 @@ export const RCmobileChat = () => {
       isClosable={true}
       setClosableState={setOpen}
       width="auto"
-      height={"30vh"}
+      height={'30vh'}
       GOOGLE_CLIENT_ID={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
       host={process.env.NEXT_PUBLIC_RC_URL}
       roomId={
         process.env.NEXT_PUBLIC_RC_ROOM_ID
           ? process.env.NEXT_PUBLIC_RC_ROOM_ID
-          : "GENERAL"
+          : 'GENERAL'
       }
       channelName="General"
       anonymousMode={true}
